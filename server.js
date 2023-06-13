8f679410c05aa7650f955817dd9a1894f1f93a0a
Gurveer Madurai <maduraigurveer@gmail.com>
2023-06-12 18:07:05 -0700

    npm debug

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 // handlebars configurations, inform express
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
