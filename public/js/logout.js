134f1b76662afc3dd8302538bbc93bd860bada11
Leandro Mangubat <leandromangubat@hotmail.com>
2023-06-12 20:18:04 -0400

    added logout function

---
+++ public/js/logout.js
@@ -0,0 +1,14 @@
+const userLogout = async () => {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/"); // When successful, load the homepage
+  } else {
+    alert("Failed to log out."); // When unsuccessful, show alert
+  }
+};
+// Add an event listener to the logout button
+document.querySelector("#logout").addEventListener("click", userLogout);
