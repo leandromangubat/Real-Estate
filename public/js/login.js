2522b0392f833c48d9364f5c6fcb9ed8a8eb2a97
Leandro Mangubat <125521671+leandromangubat@users.noreply.github.com>
2023-06-18 12:35:20 -0400

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
   // Get the values of the username and password input fields
-  const username = document.querySelector("#login-name").value.trim();
-  const password = document.querySelector("#login-pass").value.trim();
+  const username = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
   // If the input fields have values
   if (username && password) {
     // Send a POST request to the login endpoint with the input values as JSON data
