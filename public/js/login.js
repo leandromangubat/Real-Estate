255f1c3737bd7f8fb6295f0f2d0dd5648f155104
Leandro Mangubat <leandromangubat@hotmail.com>
2023-06-18 09:55:56 -0400

    Integerated login logic to front end

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,25 @@
-// Handler function for chess login form submission
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  // Get the values of the username and password input fields
-  const username = document.querySelector("#login-name").value.trim();
-  const password = document.querySelector("#login-pass").value.trim();
-  // If the input fields have values
-  if (username && password) {
-    // Send a POST request to the login endpoint with the input values as JSON data
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
     const response = await fetch("/api/users/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
-    // If the request was successful, redirect to the homepage
+
     if (response.ok) {
-      document.location.replace("/"); // When successful, load the homepage
+      document.location.replace("/");
+      alert("Successful login!");
     } else {
-      // If the request was unsuccessful, show an alert
-      alert("Failed to log in."); // When unsuccessful, show alert
+      alert("Failed to log in.");
     }
   }
 };
 
-// Event listener for the chess login form
-const loginForm = document.querySelector("#login-form");
-if (loginForm) {
-  loginForm.addEventListener("submit", loginFormHandler);
-}
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
