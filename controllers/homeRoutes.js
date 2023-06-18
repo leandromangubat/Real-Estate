46b22116fd5810841c7d8b3dcf70635ce5c3a511
Leandro Mangubat <leandromangubat@hotmail.com>
2023-06-18 14:07:44 -0400

    updated login logic

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -57,7 +57,7 @@ router.get("/login", (req, res) => {
 
 router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/dashboard");
+    res.redirect("/");
     return;
   }
 
