4cbf9b46ed617cc31e209fcc087ca2a29b1f6720
mahdi83777 <mye09@mail.aub.edu>
2023-06-18 18:39:07 +0300

    sign up updates

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,8 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
   // Get the values of the username and password input fields
-  const username = document.querySelector("#username-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const username = document.querySelector("#login-name").value.trim();
+  const password = document.querySelector("#login-pass").value.trim();
   // If the input fields have values
   if (username && password) {
     // Send a POST request to the login endpoint with the input values as JSON data
