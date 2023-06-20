6e693edc5baf03b78d8737dc1d75f42c04584b1d
Leandro Mangubat <leandromangubat@hotmail.com>
2023-06-19 22:20:36 -0400

    Updated homepage to display cards in a row

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+const handlebars = require("handlebars");
+const handlebarsHelpers = require("handlebars-helpers")();
+handlebars.registerHelper(handlebarsHelpers);
 
 //New package
 const multer = require("multer");
